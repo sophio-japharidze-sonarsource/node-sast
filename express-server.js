f7b3ad799a8091398412399a4f0cc4a97ce0852a
Phil Nash <philnash@gmail.com>
2023-08-07 17:10:03 +1000

    Making express insecure

---
+++ express-server.js
@@ -0,0 +1,29 @@
+const Express = require("express");
+const path = require("path");
+const fs = require("fs/promises");
+
+const app = Express();
+app.use(Express.urlencoded({ extended: true }));
+const port = "3000";
+const host = "localhost";
+
+app.get("/", (_req, res) => {
+  res.send("My second server!");
+});
+
+app.post("/", (req, res) => {
+  const { dir } = req.body;
+  const directory = path.join(__dirname, dir);
+  fs.access(directory)
+    .then(async () => {
+      const result = await fs.readdir(directory);
+      res.json({ files: result });
+    })
+    .catch(() => {
+      res.json({ files: [] });
+    });
+});
+
+app.listen(3000, () => {
+  console.log(`Server is running on http://${host}:${port}`);
+});
